ca1c827de3d3af6a14cc932b75455caeed85b497
UlisesFuentes2K <157264869+UlisesFuentes2K@users.noreply.github.com>
2025-05-01 17:55:05 -0600

    ChangeMenu_01-05-2025_UF

---
+++ src/app/Component/cambio-password/cambio-password.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-cambio-password',
+  standalone: true,
+  imports: [],
+  templateUrl: './cambio-password.component.html',
+  styleUrl: './cambio-password.component.css'
+})
+export class CambioPasswordComponent {
+
+}
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,24 @@
 import { Routes } from '@angular/router';
+// Importar Componentes
+import { LoginComponent } from './Component/login/login.component';
+import { RegistroComponent } from './Component/registro/registro.component';
+import { CambioPasswordComponent } from './Component/cambio-password/cambio-password.component';
+import { PerfilComponent } from './Component/perfil/perfil.component';
+import { MenuComponent } from './Component/menu/menu.component';
+import { HomeComponent } from './Component/home/home.component';
+//
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path:'login', component:LoginComponent},
+    {
+        path:'',
+        component:MenuComponent,
+        children:[
+            {path:'', redirectTo:'home', pathMatch:'full'},
+            {path:'profile', component:PerfilComponent},
+            {path:'change_password', component:CambioPasswordComponent},
+            {path:'home', component:HomeComponent}
+        ]
+    },
+    {path:'**', redirectTo:'login'}
+];
