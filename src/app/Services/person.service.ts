0a0612e83e0c0fe46ae7019adcd5b29b2bc7ed1c
UlisesFuentes2K <157264869+UlisesFuentes2K@users.noreply.github.com>
2025-05-28 21:42:43 -0600

    ConfigServicePerson_28-05-2025_UF

---
+++ src/app/Services/person.service.ts
@@ -0,0 +1,70 @@
+import { Injectable } from '@angular/core';
+import { environment } from '../../environments/environment.development';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { catchError, map, Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PersonService {
+  private readonly APi = environment.api;
+  private readonly EndPoint ="Person";
+  private readonly token = localStorage.getItem("token") || "";
+
+  constructor(private http:HttpClient) { }
+
+  // Obtener los datos de todas las personas
+  public getAllData():Observable<any>{
+    const url = [this.APi, this.EndPoint].join('/');
+    const headers = new HttpHeaders({'Content-Type':'application/json', 'Authorization':`Bearer${this.token}`});
+
+    return this.http.get(url, {headers}).pipe(
+      map(response => (response as any).$value || []),
+      catchError(error => {
+        console.error('Error al obtener los datos', error); 
+        throw error;
+      } )
+    )
+  }
+
+  // Obtener los datos de una persona
+  public getOneData(Id:number):Observable<any>{
+    const url = [this.APi, this.EndPoint, Id].join('/');
+    const headers = new HttpHeaders({'Content-Type':'application/json', 'Authorization':`Bearer${this.token}`});
+
+    return this.http.get(url, {headers}).pipe(
+      map(response => (response as any).$value || null),
+      catchError(error => {
+        console.error('Error al obtener los datos', error); 
+        throw error;
+      } )
+    )
+  }
+
+  // Guardar los datos de una persona
+  public postData(data:any):Observable<any>{
+    const url = [this.APi, this.EndPoint].join('/');
+    const headers = new HttpHeaders({'Content-Type':'application/json', 'Authorization':`Bearer${this.token}`});
+
+    return this.http.post(url, data, {headers}).pipe(
+      map(response => (response as any).$value || null),
+      catchError(error => {
+        console.error("Error al guardar los datos", error); 
+        throw error;})
+    )
+  }
+
+  // Actualizar los datos de una persona
+  public putData(data:any):Observable<any>{
+    const url = [this.APi, this.EndPoint].join('/');
+    const headers = new HttpHeaders({'Content-Type':'application/json', 'Authorization':`Bearer${this.token}`});
+
+    return this.http.put(url, data, {headers}).pipe(
+      map(response => (response as any).$value || null),
+      catchError(error => {
+        console.error('Error al actualizar los datos');
+        throw error;
+      })
+    )
+  }
+}
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,1 +1,6 @@
-export const environment = {};
+export const environment = {
+    production:false,
+    api : 'https://localhost:7201'
+};
+
+
